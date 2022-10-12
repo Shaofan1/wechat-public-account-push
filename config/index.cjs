78315990c12d9c4074e3aefdfbed523af7225511
Shaofan1 <92199646+Shaofan1@users.noreply.github.com>
2022-10-12 14:01:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o0HK_6hAPx8n1fWJul3oM4fW2Wbg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'zn8wZkjBpEKCBFq3opFzPU6ta5NomX3KLxAUTgHuqXs',
+      useTemplateId: 'AZts05dD87mOpsyCGEzEqFTwwV3FePglH-KyagfvYZo	',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-17',
       festivals: [
@@ -33,10 +33,10 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '麻椒', year: '1998', date: '12-17',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '生日', name: '烧饭', year: '1998', date: '03-17',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
