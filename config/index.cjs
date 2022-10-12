2162072a0e52127a9e0d606bf8339ac825323519
Shaofan1 <92199646+Shaofan1@users.noreply.github.com>
2022-10-12 14:28:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '麻椒同学',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o0HK_6hAPx8n1fWJul3oM4fW2Wbg',
+      id: 'o0HK_6sCXzacIwNFsVr8bKXyVG54',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'FmS08PpwWQ9fGna-4xiX3x1Ey54SdqlPrTXF6QZ1q0o',
+      useTemplateId: '	faojhPrZEBhziYNE7FZJaDQCloZ-MVDho-SAuyf_DDA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-17',
       festivals: [
